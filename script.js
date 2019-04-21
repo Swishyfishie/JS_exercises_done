5c195f3ba13bf761f5192af7f73aaabf2a65e8bd
[swishyfishie] <[pclandrei@gmail.com]>
2019-04-21 02:29:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// SET DATE AND TIME IN THIS SPECIFIC FORMAT
+
+let newDate = new Date;
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let today = days[newDate.getDay()];
+let hour = newDate.getHours();
+let ampm = hour >= 12 ? 'pm' : 'AM';
+let minutes = newDate.getMinutes();
+let seconds = newDate.getSeconds();
+
+let example = `Today is: ${today}.
+Current time is: ${hour}${ampm} : ${minutes} : ${seconds}. 
+`
+
+console.log(example);
+
+// SET DATE AND TIME IN THIS SPECIFIC FORMAT
